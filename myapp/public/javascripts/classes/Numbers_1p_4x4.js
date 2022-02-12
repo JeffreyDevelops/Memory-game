98dde70e826de9378818a1f03b5a2ea7fc5747af
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2022-02-12 23:32:51 +0100

    simplified code / got the cards flip working / for each card

--- myapp/public/javascripts/classes/Numbers_1p_4x4.js
+++ myapp/public/javascripts/classes/Numbers_1p_4x4.js
@@ -23,22 +23,22 @@ export default class Numbers_1p_4x4 {
       
       <div class="game-flex">
       <div id="game-grid-number-solo-4x4">
-       <div class="game-flex"><span>2</span></div>
-       <div class="game-flex"><span>3</span></div>
-       <div class="game-flex"><span>5</span></div>
-       <div class="game-flex"><span>4</span></div>
-       <div class="game-flex"><span>9</span></div>
-       <div class="game-flex"><span>5</span></div>
-       <div class="game-flex"><span>8</span></div>
-       <div class="game-flex"><span>7</span></div>
-       <div class="game-flex"><span>4</span></div>
-       <div class="game-flex"><span>8</span></div>
-       <div class="game-flex"><span>3</span></div>
-       <div class="game-flex"><span>6</span></div>
-       <div class="game-flex"><span>9</span></div>
-       <div class="game-flex"><span>2</span></div>
-       <div class="game-flex"><span>7</span></div>
-       <div class="game-flex"><span>6</span></div>
+       <div class="game-flex">2</div>
+       <div class="game-flex">3</div>
+       <div class="game-flex">4</div>
+       <div class="game-flex">5</div>
+       <div class="game-flex">6</div>
+       <div class="game-flex">7</div>
+       <div class="game-flex">8</div>
+       <div class="game-flex">9</div>
+       <div class="game-flex">11</div>
+       <div class="game-flex">22</div>
+       <div class="game-flex">22</div>
+       <div class="game-flex">33</div>
+       <div class="game-flex">43</div>
+       <div class="game-flex">32</div>
+       <div class="game-flex">1</div>
+       <div class="game-flex">2</div>
       </div>
       </div>
       
@@ -58,10 +58,19 @@ export default class Numbers_1p_4x4 {
 
       _click_memory() {
         let test = this.html_numbers_solo_4x4.querySelectorAll("#game-grid-number-solo-4x4 span");
-        test.forEach(e => {
-          e.style.display = "none";
-      });
+        let test1 = this.html_numbers_solo_4x4.querySelector("#game-grid-number-solo-4x4");
+        let test2 = this.html_numbers_solo_4x4.querySelectorAll("#game-grid-number-solo-4x4 .game-flex > span");
 
+        test1.addEventListener("click", e =>{
+
+          if (e.target.classList.contains("game-flex")) {
+             e.target.classList.toggle("flip");
+            
+          } else {
+            console.log("It doesn't work G");
+          }
+        });
+        
 
       }
 
