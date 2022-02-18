35726960964fac6d1e6c7f7b340b50ac1eb4b27d
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2022-02-18 14:39:55 +0100

    changed route names

--- myapp/app.js
+++ myapp/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var menuRouter = require('./routes/menu');
+var gamesRouter = require('./routes/games');
 
 var app = express();
 
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/solo_1_4x4', usersRouter);
+app.use('/', menuRouter);
+app.use('/solo_1_4x4', gamesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
