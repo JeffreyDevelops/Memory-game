66374c1749badf06b79c3db2b4cd19429c2d5f78
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2022-02-18 14:47:12 +0100

    added another router and changed descriptions

--- myapp/routes/games.js
+++ myapp/routes/games.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET number-solo-4x4. */
 router.get('/', function(req, res, next) {
   res.render("numbers-solo/numbers_1p_4x4", {title: "Numbers-Solo-4x4"});
+});
+
+/* GET number-solo-6x6. */
+router.get('/', function(req, res, next) {
+  res.render("numbers-solo/numbers_1p_6x6", {title: "Numbers-Solo-6x6"});
 });
 
 module.exports = router;
