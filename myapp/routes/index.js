e33edf67e6cba914ec5abed1c16211f71eea54c5
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2022-02-18 14:22:42 +0100

    organized files in structured folders

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('game-menu', { title: 'Menu' });
+  res.render('game-menu/game-menu', { title: 'Menu' });
 });
 
 module.exports = router;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render("numbers_1p_4x4", {title: "Numbers-Solo-4x4"});
+  res.render("numbers-solo/numbers_1p_4x4", {title: "Numbers-Solo-4x4"});
 });
 
 module.exports = router;
