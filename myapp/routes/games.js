0e2fddbe19959f6f1f3e1961b7be6e0dccef9c7d
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2022-02-20 15:34:01 +0100

    got solo working with the new folder paths

--- myapp/routes/games.js
+++ myapp/routes/games.js
@@ -5,22 +5,22 @@ var router = express.Router();
 
 /* GET number-solo-4x4. */
 router.get('/', function(req, res, next) {
-  res.render("numbers-solo/numbers_1p_4x4", {title: "Numbers-Solo-4x4"});
+  res.render("solo/numbers-solo/numbers_1p_4x4", {title: "Numbers-Solo-4x4"});
 });
 
 /* GET number-solo-6x6. */
 router.get('/6x6', function(req, res, next) {
-  res.render("numbers-solo/numbers_1p_6x6", {title: "Numbers-Solo-6x6"});
+  res.render("solo/numbers-solo/numbers_1p_6x6", {title: "Numbers-Solo-6x6"});
 });
 
 /* GET icons-solo-4x4. */
 router.get('/icons-solo-4x4', function(req, res, next) {
-  res.render("icons-solo/icons_1p_4x4", {title: "Icons-Solo-4x4"});
+  res.render("solo/icons-solo/icons_1p_4x4", {title: "Icons-Solo-4x4"});
 });
 
 /* GET icons-solo-6x6. */
 router.get('/icons-solo-6x6', function(req, res, next) {
-  res.render("icons-solo/icons_1p_6x6", {title: "Icons-Solo-6x6"});
+  res.render("solo/icons-solo/icons_1p_6x6", {title: "Icons-Solo-6x6"});
 });
 
 
